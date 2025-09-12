1eed703eae29f3320bc77c99dcf10d07239b8ddf
Abdullahi <abdullahi@osman.abdullahi@strathmore.edu>
2025-09-12 21:22:05 +0300

    updating files

---
+++ ClassAutoLoad.php
@@ -0,0 +1,22 @@
+<?php
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'Plugins/PHPMailer/vendor/autoload.php';
+require_once 'conf.php';
+
+$directories = ["Forms", "Layouts", "Global"];
+
+spl_autoload_register(function ($className) use ($directories) {
+    foreach ($directories as $directory) {
+        $filePath = __DIR__ . "/$directory/" . $className . '.php';
+        if (file_exists($filePath)) {
+            require_once $filePath;
+            return;
+        }
+    }
+});
+
+// create an instance of HelloWorld
+$ObjSendMail = new SendMail();
+$ObjForm = new forms();
+$ObjLayout = new layouts();
---
+++ conf.php
@@ -0,0 +1,15 @@
+<?php
+// Site Information
+$conf['site_name'] = 'ICS C Community';
+$conf['site_url'] = 'http://localhost';
+$conf['admin_email'] = 'osman.abdullahi@strathmore.edu';
+
+// Database Configuration
+$conf['db_type'] = 'pdo';
+$conf['db_host'] = 'localhost';
+$conf['db_user'] = 'root';
+$conf['db_pass'] = 'qwerty123';
+$conf['db_name'] = 'dbtest1';
+
+// Site Language
+$conf['site_lang'] = 'en';
