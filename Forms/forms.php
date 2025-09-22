6342d85aa7ceecdf466507eba6006dc57275abb9
Abdullahi <abdullahi@osman.abdullahi@strathmore.edu>
2025-09-22 13:28:26 +0300

    final changes

--- Forms/forms.php
+++ Forms/forms.php
@@ -2,31 +2,51 @@
 class forms {
     public function signup() {
 ?>
-    <form method="post" action="">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        <br><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <br><br>
-        <input type="submit" value="Sign Up"> <a href="signin.php">Already have an account? Log in</a>
-    </form>
+<h2 class="text-center mb-3" style="color:#007e85;">Sign Up</h2>
+<form method="post">
+  <div class="mb-3">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" name="username" required>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" required>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password" required>
+  </div>
+  <?php $this->submit_button("Sign Up", "signup"); ?>
+  <p class="text-center mt-3">Already have an account? <a href="login.php" style="color:#007e85;">Login</a></p>
+</form>
 <?php
     }
-    public function login() {
-        ?>
-        <form method="post" action="">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            <br><br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            <br><br>
-            <input type="submit" value="Log In"> <a href="./">Don't have an account? Sign up</a>
-        </form>
-        <?php
+
+    public function signin() {
+?>
+<h2 class="text-center mb-3" style="color:#007e85;">Login</h2>
+<form method="post">
+  <div class="mb-3">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" name="username" required>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password" required>
+  </div>
+  <?php $this->submit_button("Login", "login"); ?>
+  <p class="text-center mt-3">Don’t have an account? <a href="register.php" style="color:#007e85;">Sign Up</a></p>
+</form>
+<?php
+    }
+
+    private function submit_button($value, $name) {
+?>
+<button type="submit" class="btn btn-primary w-100" 
+        style="background-color:#007e85; border-color:#007e85;" 
+        name="<?php echo $name; ?>" value="<?php echo $value; ?>">
+    <?php echo $value; ?>
+</button>
+<?php
     }
 }
